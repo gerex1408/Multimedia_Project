5d5244ff7f77ff258f30c4b686e8590cb95aee53
Gerard <geriloza@gmail.com>
2021-11-17 18:38:32 +0100

    shoe cards almost done

--- loadShoes.js
+++ loadShoes.js
@@ -1,9 +1,9 @@
-let colorFilter
-let sizeFilter
-let minPrice
-let maxPrice
-let styleFilter
-let brandFilter
+var colorFilter
+var sizeFilter
+var minPrice
+var maxPrice
+var styleFilter
+var brandFilter
 
 
 function getFiltersValues(){
@@ -48,26 +48,27 @@ function filterShoes(shoesArr){
                             nShoes++;
                             shoesContainer.innerHTML+=`
                             <div class="shoeCard">
-                            <div class="mainInfo">
-                              <img class="logo" src="https://wallpapercave.com/wp/CgjGnw0.jpg" alt="">
-                              <h3 class="shoeName">Nike</h3>
-                            </div>
-                            <img class="shoe" src="https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2020%2F02%2Fnike-top-10-best-selling-sneakers-list-2019-0-tw.jpg?w=960&cbr=1&q=90&fit=max" alt="">
-                              <div class="size">
-                                <p class="label">Size:</p>
-                                <ul class="sizes">
-                                  <li class="sizeNumber">9</li>
-                                  <li class="sizeNumber">10</li>
-                                  <li class="sizeNumber">10.5</li>
-                                  <li class="sizeNumber">11</li>
-                                  <li class="sizeNumber">11.5</li>
-                                </ul>
-                              </div>
-                              <div class="tags">
-                                <button class="cartButton">Add to Cart</button>
-                                <p class="price">200€</p>
-                              </div>
-                            </div>
+                                <div class="mainInfo">
+                                    ${
+                                        shoe.brand==='NIKE'?`<img class="logo" src="./images/nike.jpg" alt="logo"></img>`:
+                                        shoe.brand==="ADIDAS"?`<img class="logo" src="./images/adidas.jpg" alt="logo"></img>`:
+                                        shoe.brand==="PUMA"&&`<img class="logo" src="./images/puma.png" alt="logo"></img>`
+                                    }
+                                    <h3 class="shoeName">${shoe.name}</h3>
+                                </div>
+                                <div class="imageDiv">
+                                    <img class="shoe" src="./images/shoe${shoe.id}.jpg" alt="shoe image">
+                                </div>
+                                <div class="size">
+                                    <p class="label">Size:</p>
+                                    <ul class="sizes">
+                                        ${shoe.sizes.map(size => `<li class="sizeNumber">${size}</li>`).join("")}
+                                    </ul>
+                                </div>
+                                <div class="tags">
+                                        <button class="cartButton">Add to Cart</button>
+                                        <p class="price">${shoe.price}€</p>
+                                </div>
                           </div>`
                         }
                     }
--- shoesDictionary.js
+++ shoesDictionary.js
@@ -1,25 +1,28 @@
 const menShoes = [
     {
+        id:1,
         name:"Nike pro Max",
         brand:"NIKE",
         color:"White",
-        sizes:[40,41,42,43],
+        sizes:[40,41,42,43,44],
         price:120,
         style:"Sport",
     },
     {
+        id:2,
         name:"Adidas iluminati",
         brand:"ADIDAS",
         color:"Black",
-        sizes:[36,37,38,39],
+        sizes:[36,37,38,39,40],
         price:200,
         style:"Casual",
     },
     {
+        id:3,
         name:"Puma wild",
         brand:"PUMA",
         color:"Red",
-        sizes:[40,41,42,43],
+        sizes:[40,41,42,43,44],
         price:80,
         style:"Casual",
 
