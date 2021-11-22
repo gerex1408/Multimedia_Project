b726d48665e20396d5d194db1cf5e210c52cd97a
Gerard <geriloza@gmail.com>
2021-11-22 19:39:15 +0100

    falta womenShoes, kidsShoes i el carrito

---
+++ JS/loadShoes.js
@@ -0,0 +1,187 @@
+var colorFilter
+var sizeFilter
+var minPrice
+var maxPrice
+var styleFilter
+var brandFilter
+var shoesArr
+
+
+function getFiltersValues(){
+    colorFilter = document.getElementById("colorSelect").value;
+    sizeFilter = document.getElementById("sizeSelect").value;
+    minPrice = document.getElementById("priceFrom").value;
+    maxPrice = document.getElementById("priceTo").value;
+    styleFilter = document.getElementById("styleSelect").value;
+    brandFilter = document.getElementById("brandSelect").value;
+}
+
+function addToCart(id){
+    let size = document.getElementsByClassName("chosen");
+    if(size.length===0){
+        console.log("error")
+        //s'avisa i no s'afegeix al carro
+    }
+    else{
+        size=size[0].value;
+        console.log(id,size)
+        //s'afegira a la bossa la sabata amb aquesta mida
+    }
+    //afegir al local storage i al carrito.
+}
+
+function selectSize(id,size){
+    let lis = document.getElementById("sizeList").children
+    for(let i of lis){
+        i.classList.remove("chosen")
+        i.classList.add("selected")
+    }
+    let li = document.getElementById("id"+id+"size"+size)
+    li.classList.remove("selected")
+    li.classList.add("chosen")
+}
+
+function showModal(id){
+    
+    let shoe = shoesArr.find(s=>s.id===id);
+    document.getElementById("uniqueContent").innerHTML=`
+    <div class="center">
+        <section class="card">
+            <img id="shoe-target" class="shoes" src="../images/shoes/unique${shoe.id}.png" alt="shoe img" />
+        </section>
+        <section class="cardTwo">
+            <h1 class="shoe-title">${shoe.name}</h1>
+            <div class="price-listing">
+                <span class="price">${shoe.price}€</span>
+            </div>
+            <p class="shoe-detail">
+                ${shoe.description}
+            </p>
+            <div class="size">
+                <p class="label">Size:</p>
+                <ul id="sizeList">
+                ${shoe.sizes.map(size => `<li value=${size} class="selected" id="id${shoe.id}size${size}" onclick="selectSize(${shoe.id},${size})">${size}</li>`).join("")}
+                </ul>
+            </div>
+            <div class="size">
+                <p class="label">Color:</p>
+                <ul>
+                    <li>${shoe.color}</li>
+                </ul>
+            </div>
+            <div class="size">
+                <p class="label">Style:</p>
+                <ul>
+                    <li>${shoe.style}</li>
+                </ul>
+            </div>
+            <div class="button-container">
+                <button class="primary" data-toggle="modal" data-target=".bd-example-modal-lg" onclick="addToCart(${shoe.id})">Add to Cart</button>
+            </div>
+        </section>
+        </div>
+    </div>
+    `
+    var bouncingShoes = anime({
+        targets: '#shoe-target',
+        translateY: '15',
+        duration: 2000,
+        loop: true,
+        direction: 'alternate',
+        easing: 'linear'
+    });
+    document.getElementById("shoeModal").style.display="block"
+}
+
+function filterShoes(){
+    const shoesContainer = document.getElementById("shoesRow");
+    shoesContainer.innerHTML=''
+
+    let sizeArr=[]
+    if(sizeFilter!==''){
+        const minSize = sizeFilter.split('-')[0];
+        const maxSize = sizeFilter.split('-')[1];
+    
+        let diffBetweenSizes = maxSize-minSize;
+    
+        for(let i=0;i<=diffBetweenSizes;i++){
+            sizeArr.push(parseInt(minSize)+i)
+        }
+    }
+    
+    let nShoes = 0;
+    for(let shoe of shoesArr){
+        if(colorFilter===shoe.color || colorFilter===''){
+            let hasTheSize=false;
+            for(let s of sizeArr){
+                if(shoe.sizes.includes(s)){
+                    hasTheSize=true;
+                    break
+                }
+            }
+            if(hasTheSize||sizeFilter===''){
+                if((shoe.price>=minPrice && shoe.price<=maxPrice)||(minPrice==='' && maxPrice==='')){
+                    if(styleFilter===shoe.style || styleFilter===''){
+                        if(brandFilter===shoe.brand || brandFilter===''){
+                            nShoes++;
+                            shoesContainer.innerHTML+=`
+                            <div class="shoeCard">
+                                <div class="mainInfo">
+                                    ${
+                                        shoe.brand==='NIKE'?`<img class="logo" src="../images/logos/nike.jpg" alt="logo"></img>`:
+                                        shoe.brand==="ADIDAS"?`<img class="logo" src="../images/logos/adidas.jpg" alt="logo"></img>`:
+                                        shoe.brand==="PUMA"?`<img class="logo" src="../images/logos/puma.png" alt="logo"></img>`:
+                                        shoe.brand==="JORDAN"&&`<img class="logo" src="../images/logos/jordan.jpg" alt="logo"></img>`
+                                    }
+                                    <h4 class="shoeName">${shoe.name}</h4>
+                                </div>
+                                <div class="imageDiv">
+                                    <img class="shoe" src="../images/shoes/shoe${shoe.id}.jpg" alt="shoe image" data-toggle="modal" data-target=".bd-example-modal-lg" onclick="showModal(${shoe.id})">
+                                </div>
+                                <div class="size">
+                                    <p class="label">Size:</p>
+                                    <ul class="sizes">
+                                        ${shoe.sizes.map(size => `<li class="sizeNumber">${size}</li>`).join("")}
+                                    </ul>
+                                </div>
+                                <div class="tags">
+                                        <button class="cartButton" data-toggle="modal" data-target=".bd-example-modal-lg" onclick="showModal(${shoe.id})">Add to Cart</button>
+                                        <p class="price">${shoe.price}€</p>
+                                </div>
+                          </div>`
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    if(nShoes===0){
+        shoesContainer.innerHTML=`<p>NO SHOES MATCHING THIS FILTER</p>`
+    }
+}
+
+function menShoesLoad(){
+    getFiltersValues()
+    shoesArr=menShoes
+    filterShoes()
+}
+function womenShoesLoad(){
+    getFiltersValues()
+    shoesArr=womenShoes
+    filterShoes()
+}
+function kidsShoesLoad(){
+    getFiltersValues()
+    shoesArr=kidsShoes
+    filterShoes()
+}
+
+function clearFilters(shoes){
+    for(let element of document.getElementsByClassName("form-control")){
+        element.value='';
+    }
+    getFiltersValues()
+    shoesArr=shoes
+    filterShoes()
+}
--- JS/shoesDictionary.js
+++ JS/shoesDictionary.js
@@ -7,6 +7,7 @@ const menShoes = [
         sizes:[40,41,42,43,44],
         price:129.99,
         style:"Casual",
+        description:"Designed to fit the feet and offer comfort, the Nike Air Huarache brings back a favorite urban model. The soft synthetic leather on the upper is combined with a super breathable neoprene fabric with a perfect shine to offer a simple style."
     },
     {
         id:2,
@@ -16,6 +17,7 @@ const menShoes = [
         sizes:[36,37,38,39,40],
         price:140,
         style:"Sport",
+        description:"Discover new corners of the city or do something you have never done before. Take on the unpredictable with the adidas NMD_R1. Launched in 2015, this shoe combines a design inspired by retro running footwear with the most innovative technologies."
     },
     {
         id:3,
@@ -25,6 +27,7 @@ const menShoes = [
         sizes:[40,41,42,43,44],
         price:110,
         style:"Casual",
+        description:"PUMA has your fashion needs fully covered with the RS-Z College sneakers, the latest innovation in streetwear. This new addition to the RS family features an elegant silhouette, accented with bold colors, that will turn heads."
 
     },
     {
@@ -35,6 +38,7 @@ const menShoes = [
         sizes:[40,41,42,43,44],
         price:199.99,
         style:"Sport",
+        description:"Designed for strength and stability, the Nike Romaleos 4 features a supportive midsole and a wide, flat outsole. Adjustable straps in the midfoot support your foot through the most intense workouts."
 
     },
     {
@@ -45,6 +49,7 @@ const menShoes = [
         sizes:[45,46,47,48,49],
         price:90,
         style:"Casual",
+        description:"Enjoy extraordinary comfort and superior functionality with the Tarrenz SB shoes, with an all-over leather upper complete with PURE-TEX, a waterproof and breathable membrane that keeps shoes looking new all year long."
 
     },
     {
@@ -55,6 +60,7 @@ const menShoes = [
         sizes:[38,39,40,41,42],
         price:100,
         style:"Casual",
+        description:"Do you remember the year 1991? Or maybe not. With this adidas Gazelle shoe you will feel like traveling back in time. This re-edition of the original revives the spirit of the 90s with a soft upper and contrasting 3-Stripes."
 
     },
     {
@@ -65,6 +71,7 @@ const menShoes = [
         sizes:[42,43,45,48,50],
         price:119.99,
         style:"Casual",
+        description:"Inspired by the early AJ1s, the Air Jordan 1 Mid sneakers offer fans of retro Jordan the opportunity to follow in the footsteps of greatness. They are just a classic !"
 
     },
     {
@@ -75,6 +82,7 @@ const menShoes = [
         sizes:[44,45,46,47,48],
         price:119.99,
         style:"Sport",
+        description:"Zion is unlike anything or anyone thanks to his down-to-earth demeanor and near-alien capabilities. On the court, his gracious personality that puts family top priority transforms into an unstoppable strength of fitness and limitless speed. Zion 1 are the first of its exclusive line."
 
     },
 ]
